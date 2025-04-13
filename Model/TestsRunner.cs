f5b4b7c7e39d3a1b0d15bee8379536ad787ef991
bmirov <bmirov@BMIROV>
2025-04-13 09:13:13 -0400

    Add project files.

---
+++ Model/TestsRunner.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace SystemAutomationTesting.Model
+{
+    class TestsRunner
+    {
+        private List<CheckboxItem> _selectedTests;
+        private List<Device> _devices;
+
+        public TestsRunner(List<CheckboxItem> selectedTests, List<Device> devices)
+        {
+            _selectedTests = selectedTests;
+            _devices = devices;
+        }
+
+        // Optional: Add methods to interact with the test objects
+        public List<CheckboxItem> GetTestObjects()
+        {
+            return _selectedTests;
+        }
+
+        public void RunTests()
+        {
+            foreach (var device in _devices)
+            {
+                Debug.WriteLine($"Device Index: {device.DeviceIndex}, Device Type: {device.DeviceType}, Parameter: {device.Parameter}, Value: {device.Value}");
+            }
+            foreach (var selectedTest in _selectedTests)
+            {
+                Debug.WriteLine($"Selected Test: {selectedTest.Name}, Is Checked: {selectedTest.IsChecked}");
+                //try
+                //{
+                //    // Get the type of the class with the same name as the selectedTest.Name
+                //    var type = Type.GetType($"SystemAutomationTesting.Model.{selectedTest.Name}");
+
+                //    if (type == null)
+                //    {
+                //        Debug.WriteLine($"Class '{selectedTest.Name}' not found.");
+                //        throw new InvalidOperationException($"Class '{selectedTest.Name}' not found in the namespace 'SystemAutomationTesting.Model'.");
+                //    }
+
+                //    // Define the arguments to pass to the constructor
+                //    object[] constructorArgs = { /* Add your arguments here */ };
+
+                //    // Dynamically create an instance of the class with the arguments
+                //    var instance = Activator.CreateInstance(type, constructorArgs);
+
+                //    // Optional: Call a method on the instance if needed
+                //    Debug.WriteLine($"Successfully created an instance of {type.Name}");
+                //}
+                //catch (Exception ex)
+                //{
+                //    Debug.WriteLine($"Error instantiating class '{selectedTest.Name}': {ex.Message}");
+                //    throw;
+                //}
+            }
+        }
+    }
+}
---
+++ ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Windows.Input;
+using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
+using SystemAutomationTesting.Model;
+using System.Diagnostics;
+using System.Text;
+
+public class Device
+{
+    public int DeviceIndex { get; set; }
+    public string DeviceType { get; set; }
+    public string Parameter { get; set; }
+    public string Value { get; set; }
+}
+
+public class MainWindowViewModel
+{
+    public ObservableCollection<CheckboxItem> CheckboxItems { get; set; }
+    public ICommand ProcessSelectedCommand { get; }
+    public ObservableCollection<Device> Devices { get; set; }
+    public MainWindowViewModel()
+    {
+
+        // Initialize command
+        ProcessSelectedCommand = new RelayCommand(ProcessSelected);
+        // Initialize checkbox items
+        CheckboxItems = new ObservableCollection<CheckboxItem>
+            {
+                new CheckboxItem { Name = "ALL", IsChecked = false },
+                new CheckboxItem { Name = "Test 1", IsChecked = false },
+                new CheckboxItem { Name = "Test 2", IsChecked = false }
+            };
+
+
+        // Example data
+        Devices = new ObservableCollection<Device>
+        {
+            new Device { DeviceIndex = 1, DeviceType = "Sensor", Parameter = "Temperature", Value = "25°C" },
+            new Device { DeviceIndex = 2, DeviceType = "Actuator", Parameter = "Position", Value = "50%" },
+            new Device { DeviceIndex = 3, DeviceType = "Controller", Parameter = "Mode", Value = "Auto" }
+        };
+
+    }
+
+    private void ProcessSelected()
+    {
+        // Get selected checkboxes
+        var selectedItems = CheckboxItems.Where(item => item.IsChecked).ToList();
+        // Get the updated Devices list
+        var updatedDevices = Devices.ToList();
+
+        TestsRunner testsRunner = new TestsRunner(selectedItems, updatedDevices);
+        testsRunner.RunTests();
+    }
+
+
+}
